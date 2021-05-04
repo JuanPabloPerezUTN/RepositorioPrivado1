f4788ec077cb21fd1b2e3de5276dac2db25bd454
JuanPabloPerezUTN <83587680+JuanPabloPerezUTN@users.noreply.github.com>
2021-05-04 09:44:56 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+
+    double b;
+    b-1000;
+
+    double c;
+    c=3600;
+
+    cout<<"ingrese un valor en Km/h"<<endl;
+    cout<<""<<endl;
+
+    cint>> a;
+
+    cout<<""<<endl;
+    cout<<"el valor en m/s es:" <<a * b /c << "m/s" <<endl;
+    system("pause>NULL");
+    return 0;
+
+}
